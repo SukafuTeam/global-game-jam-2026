4f2298da8b6a58f491979f2e9a06ee612d1d488f
andref-dev <andre.hl2@outlook.com>
2026-02-01 22:46:18 -0300

    - Adds icon

--- src/menu_scene.gd
+++ src/menu_scene.gd
@@ -7,3 +7,6 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("ui_accept"):
 		get_tree().change_scene_to_file("res://scenes/tuto_scene.tscn")
+	
+	if Input.is_action_just_pressed("dash") or Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
